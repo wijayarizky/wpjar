725081d2b6dc1ce8c2f987630a765cf8a53e6a95
Rizky WIjaya <wayanrizkywijaya@gmail.com>
2018-04-14 14:23:55 -0700

    #test

---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<hrml>
+	<head>
+		<title>Simple Code</title>
+		
+		<!-- Bootstrap -->
+		<link href="assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+		
+		<!-- js 3.3.1 -->
+		<script src="assets/js/jquery-3.3.1.js"></script>
+		<script src="assets/js/jquery-3.3.1.min.js"></script>
+		
+		<!-- Bootstrap js -->
+		<script src="assets/bootstrap/js/bootstrap.min.js"></script>
+		
+		
+	</head>
+	<body>
+		<?php include 'build/header.php' ?>
+		
+		<!-- get content action -->
+		<?php
+			$action = "";
+			$content = "build/content/home.php";
+			if ($_SERVER["QUERY_STRING"] != null){
+				$action = $_GET['a'];	
+			}
+			switch ($action){
+				case "home":
+				$content = "build/content/home.php";
+				break;
+				case "course":
+				$content = "build/content/course.php";
+				break;
+				case "about":
+				$content = "build/content/about.php";
+				break;
+			}
+		?>
+		
+		<!-- content view -->
+		<div class="container">
+		<?php 
+			if($content!=""){
+				include $content ;
+			}
+		?>
+		</div>
+	</body>
+	<?php include 'build/footer.php' ?>
+</html>
+<script type="text/javascript">
+	function getDom(){
+		var domain = $('#domain').val();
+		if(domain != ''){
+			getLookup(domain);
+		}
+	}
+	
+	function getLookup(domain){
+		$.ajax({
+			type:"POST",
+			url:"build/content/lookup.php",
+			data:{"domain":domain},
+			success:function(data){
+				$("#hasil_lookup").html(data);
+			}
+		});
+	}
+</script>
