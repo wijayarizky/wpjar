fb8e2841a1ec4dbd51f3175795e34b9236c09a7f
Rizky WIjaya <wayanrizkywijaya@gmail.com>
2018-04-18 10:54:58 -0700

    #update content tugas home

--- build/content/home.php
+++ build/content/home.php
@@ -25,6 +25,70 @@
 	</div>
 </div>
 
+<hr class="featurette-divider">
+
+<div class="row">
+	<div class="col-md-4">
+		 <div class="card mb-4 box-shadow" style="border: 1px solid; border-radius: .25rem; margin-bottom: 1.5rem;">
+			<img class="card-img-top" src="assets/img/card/1.jpg" alt="Card image cap">
+			<div class="card-body" style="padding: 1.25rem; word-wrap: break-word;">
+				<h4 class="card-title"><b>Tugas 1</b></h4>
+				<p class="card-text" align="justify">Beberapa hal mengenai Debian OS berdasarkan debian handbook by Raphael Hertzog dan Roland Mas.</p>
+				<div class="d-flex justify-content-between align-items-center">
+					<div class="btn-group">
+						<button type="button" class="btn btn-primary">View</button>
+					</div>
+				</div>
+			</div>
+		 </div>
+	</div>
+	<div class="col-md-4">
+		 <div class="card mb-4 box-shadow" style="border: 1px solid; border-radius: .25rem; margin-bottom: 1.5rem;">
+			<img class="card-img-top" src="assets/img/card/1.jpg" alt="Card image cap">
+			<div class="card-body" style="padding: 1.25rem; word-wrap: break-word;">
+				<h4 class="card-title"><b>Tugas 2</b></h4>
+				<p class="card-text" align="justify">Contoh routing static router cisco dan penerapan metric pada masing - masing router menggunakan cisco packet tracert.</p>
+				<div class="d-flex justify-content-between align-items-center">
+					<div class="btn-group">
+						<button type="button" class="btn btn-primary">View</button>
+					</div>
+				</div>
+			</div>
+		 </div>
+	</div>
+	<div class="col-md-4">
+		 <div class="card mb-4 box-shadow" style="border: 1px solid; border-radius: .25rem; margin-bottom: 1.5rem;">
+			<img class="card-img-top" src="assets/img/card/1.jpg" alt="Card image cap">
+			<div class="card-body" style="padding: 1.25rem; word-wrap: break-word;">
+				<h4 class="card-title"><b>Tugas 3</b></h4>
+				<p class="card-text" align="justify">Contoh routing static router cisco dengan pembaian IP yang telah disubnet menggunakan cisco packet tracert.</p>
+				<div class="d-flex justify-content-between align-items-center">
+					<div class="btn-group">
+						<button type="button" class="btn btn-primary">View</button>
+					</div>
+				</div>
+			</div>
+		 </div>
+	</div>
+	<div class="col-md-4">
+		 <div class="card mb-4 box-shadow" style="border: 1px solid; border-radius: .25rem; margin-bottom: 1.5rem;">
+			<img class="card-img-top" src="assets/img/card/1.jpg" alt="Card image cap">
+			<div class="card-body" style="padding: 1.25rem; word-wrap: break-word;">
+				<h4 class="card-title"><b>Tugas 4</b></h4>
+				<p class="card-text" align="justify">Penjelasan Network seurity Challenges berdasarkan pada modul 1 CEH (Certificate Ethical Hacker) version 7 by EC - Council.</p>
+				<div class="d-flex justify-content-between align-items-center">
+					<div class="btn-group">
+						<button type="button" class="btn btn-primary">View</button>
+					</div>
+				</div>
+			</div>
+		 </div>
+	</div>
+</div>
+
+<hr class="featurette-divider">
+
+
 
 
 
